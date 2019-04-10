36cf5b00f49b21000d9225c5c627e801d30d466b
Rudy Ravelin <rudy@noukod.com>
2019-04-10 17:08:27 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,14 +1,14 @@
-// .src/containers/MoviesPage.js
+// ./src/containers/MoviesPage.js
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 import MovieShow from './MovieShow';
-
-const MoviesPage = ({ match, movies }) => (
+ 
+const MoviesPage = ({ movies }) => (
   <div>
     <MoviesList movies={movies} />
-  </div>;
-
+  </div>
 )
-
+ 
 export default MoviesPage
+ 
