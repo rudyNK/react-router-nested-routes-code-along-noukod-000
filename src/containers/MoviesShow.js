25dca868e1e88470157f4dfaf20e9f51192c7479
Rudy Ravelin <rudy@noukod.com>
2019-04-10 17:13:11 +0000

    Automatically backed up by Learn

--- src/containers/MoviesShow.js
+++ src/containers/MoviesShow.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
-const MovieShow = props => {
-
+ 
+const MovieShow = ({match, movies}) => {
   return (
     <div>
-      <h3>Movies Show Component!</h3>
+      <h3>{ movies[match.params.movieId].title }</h3>
     </div>
   );
 }
-
+ 
 export default MovieShow;
